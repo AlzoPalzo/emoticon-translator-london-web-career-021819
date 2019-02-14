0bfd64b0aac2a22719f67a3d639ab7f12e1a8323
Ali Madni <ali555m@hotmail.com>
2019-02-14 22:53:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(path, emoticon)
   if library["get_emoticon"].has_key(emoticon)
     library["get_emoticon"][emoticon]
   else
-    "Sorry, that emoticon was not found"    
+    "Sorry, that emoticon was not found"
   end
 end
 
