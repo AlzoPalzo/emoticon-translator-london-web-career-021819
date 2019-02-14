831fdd123109f8f55dce4785b0233099dfcdd5e1
Ali Madni <ali555m@hotmail.com>
2019-02-14 22:38:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,8 @@ def load_library(path)
   new_lib = {"get_meaning" => {}, "get_emoticon" => {}}
 
   library.each do |meaning, emoticons|
+    new_lib["get_meaning"][emoticons[1]] = meaning
+  end
 end
 
 def get_japanese_emoticon
@@ -16,3 +18,4 @@ end
 def get_english_meaning
   # code goes here
 end
+
